931235b04dcb13d1baf70644ccc4b48b71cd65ae
Azria Florian <florian.azria@ynov.com>
2026-04-10 20:16:24 +0200

    rectif

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,10 +1,45 @@
-const guestBtn = document.getElementById("guestContinue");
-if (guestBtn) {
-  guestBtn.addEventListener("click", () => {
-    if (document.referrer) {
-      window.location.href = document.referrer;
-    } else {
-      window.history.back();
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', async () => {
+  const prenom = document.getElementById('firstName').value;
+  const nom = document.getElementById('lastName').value;
+  const email = document.getElementById('email').value;
+  const mot_de_passe = document.getElementById('password').value;
+
+    // Check si le fdp a bien tout remplie sinon on affiche une alerte pour lui dire que tous les champs sont obligatoires
+  if (!prenom || !nom || !email || !mot_de_passe) {
+    alert('Tous les champs sont obligatoires');
+    return;
+  }
+  // envoie la requete pour creer le nouvelle utilisateur
+  try {
+    const response = await fetch('http://localhost:3000/api/utilisateurs/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prenom,
+        nom,
+        email,
+        mot_de_passe
+      })
+    });
+
+    const data = await response.json();
+    // 
+    if (!response.ok) {
+      alert(data.message);
+      return;
     }
-  });
-}
+
+    alert('Compte créé avec succès');
+
+    // redirige vers la connexion
+    window.location.href = 'login.html';
+    // affiche si il y a une erreur
+  } catch (error) {
+    console.error(error);
+    alert('Erreur serveur');
+  }
+});
