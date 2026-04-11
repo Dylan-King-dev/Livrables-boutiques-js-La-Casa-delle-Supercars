d8dfd1ea36fd850b2f12ef14bfe6f2844c8e1752
Azria Florian <florian.azria@ynov.com>
2026-04-11 11:41:36 +0200

    ajout register a régler pour la comfirmation

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
+// les variable 
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
 const db = require('./db');
+
 const produitsRoutes = require('./routes/produits');
 const registerRoutes = require('./routes/back_register');
+const loginRoutes = require('./routes/back_login');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,15 +15,13 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Routes API
+// Routes API (endpoints (fin URL aprés http://localhost:3000 ex:http://localhost:3000/api/produits affiche tout les produit de la table produits))
 app.use('/api/produits', produitsRoutes);
 app.use('/api/utilisateurs', registerRoutes);
+app.use('/api/utilisateurs', loginRoutes);
 
-// Routes test
-app.get('/', (req, res) => {
-	res.json({ message: 'Backend La Casa delle Supercars is running' });
-});
 
+// Démarrage du serveur
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
 });
